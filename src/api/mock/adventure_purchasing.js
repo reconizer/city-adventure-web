0608a3e50be460723ae2c27a463a164edc6a0001
Nitwoe <szymon.licau@gmail.com>
2018-11-21 15:27:11 +0100

    Fix

--- src/api/mock/adventure_purchasing.js
+++ src/api/mock/adventure_purchasing.js
@@ -3,7 +3,6 @@ import moment from 'moment'
 export default (adventureId, startDate, endDate) => new Promise((resolve) => {
   setTimeout(() =>{
     let statData = [];
-    let sum = 0;
 
     let start = moment(startDate);
     let end = moment(endDate);
@@ -11,19 +10,15 @@ export default (adventureId, startDate, endDate) => new Promise((resolve) => {
     let diffDays = end.diff(start);
 
     for(let i = 0; i < diffDays; i++) {
-      let value = Math.floor(Math.random() * 15);
-      sum += value;
-
       statData.push({
         timestamp: start.add(i, 'days').toDate().getTime(),
-        data: value
+        data: Math.floor(Math.random() * 15)
       });
     }
 
     resolve({
       data: {
-        statistics: statData,
-        total: sum
+        statistics: statData
       }
     });
   }, 500);
