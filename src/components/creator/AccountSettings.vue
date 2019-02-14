870ffe5e3e0a1f9096369fc5895d6b33edc942e0
Nitwoe <szymon.licau@gmail.com>
2019-02-14 15:49:44 +0100

    Translations

--- src/components/creator/AccountSettings.vue
+++ src/components/creator/AccountSettings.vue
@@ -3,28 +3,28 @@
     Loader(v-if="loading")
 
     .account-settings
-      .account-settings__header Account Settings
+      .account-settings__header {{ $t("account_settings.title") }}
 
       .row(v-if="account.name")
         .col-1-2
           .row
             .col-1-3
               .form-control
-                label.form-label Profile Picture
+                label.form-label {{ $t("account_settings.profile_picture") }}
 
-                FileUpload.file-upload--square(@filesAdded="avatarChanged")
+                FileUpload.file-upload--square(@filesAdded="avatarChanged" :title="$t('account_settings.add_profile_picture')")
                   img.file-upload__image(slot="placeholder" v-if="account.avatar_url" :src="account.avatar_url")
 
             .col-2-3
               .form-control
-                label.form-label.form-label--required User Name
+                label.form-label.form-label--required {{ $t("account_settings.name") }}
                 label.error-label(v-if="error && error.name") {{ error.name.join(', ') }}
-                input.form-input(type="text" placeholder="User Name" v-model="account.name")
+                input.form-input(type="text" :placeholder="$t('account_settings.name')" v-model="account.name")
 
               .form-control
-                label.form-label.form-label--required Description
+                label.form-label {{ $t("account_settings.description") }}
                 label.error-label(v-if="error && error.description") {{ error.description.join(', ') }}
-                textarea.form-input(placeholder="Description" v-model="account.description")
+                textarea.form-input(:placeholder="$t('account_settings.description')" v-model="account.description")
 
           .row
             .col-2-3.col-offset-1-3
